5521973cd0bae607baed65e07e3d448f141fdf07
Pavel-Sedlovec <pasha.sedlovetz@gmail.com>
2026-06-10 22:14:25 +0300

    Update parsermath.h

--- include/parsermath.h
+++ include/parsermath.h
@@ -1,9 +1,10 @@
-#include <string>
-
 #ifndef PARSERMATH_H
 #define PARSERMATH_H
 
-double accept_func(std::string funcName, double x);
+#include <string>
+#include "core.h"
+
+ParseResult accept_func(std::string funcName, double x);
 double my_log(double x);
 double my_ln(double x);
 double my_sin(double x);
